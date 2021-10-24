46544931f3c83371f93b2f202fd03cb750302749
Samer Odeh <samerodeh220@gmail.com>
2021-10-24 18:51:45 +0300

    Cryptography

--- tests/test_cryptography.py
+++ tests/test_cryptography.py
@@ -1,5 +1,21 @@
 from cryptography import __version__
-
+from cryptography.cryptography import *
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def test_encrypt():
+    actual = encrypt("I Like Python", 6)
+    expected = "o roqk veznut"
+    assert actual == expected
+
+def test_decrypt():
+    actual = decrypt("o roqk veznut", 0)
+    expected = "o roqk veznut"
+    assert actual == expected 
+
+def test_crack():
+    actual = crack(encrypt("I Like Python", 3))
+    expected = "i like python"
+    assert actual == expected
+
